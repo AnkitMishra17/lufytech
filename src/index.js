d8b4720b70cd4944381e2cef8fe851855208b7d1
Ankit Mishra <ankit618731@gmail.com>
2019-08-25 06:04:53 +0530

    second commit

--- src/index.js
+++ src/index.js
@@ -34,7 +34,7 @@ app.set("views", viewsPath)
 app.use('/public',express.static(path.join(__dirname, "../public")));
 
 app.get('/', (req,res) =>{
-     res.render('employee');
+     res.render('employee', {employee: " "});
 });
 
 app.get('/viewemployee', (req,res) =>{
@@ -45,11 +45,36 @@ app.get('/viewemployee', (req,res) =>{
 });
 
 app.post('/', (req,res) =>{
-    let newemployee = Employee(req.body).save((err,data)=>{
-        if (err) throw err;
-        res.render('employee');
-    })
-});
+    console.log(req.body);
+    if (req.body._id != ''){
+        updateRecord(req, res);
+    }
+    else{
+        
+    var employee = new Employee();
+    employee.Name = req.body.Name;
+    employee.Email = req.body.Email;
+    employee.Phone = req.body.Phone;
+    employee.Age = req.body.Age;
+    employee.Address = req.body.Address;
+        employee.save((err,data)=>{
+            if (err) throw err;
+            res.render('employee', {employee: " "});    
+        })
+    }
+    });
+
+    function updateRecord(req, res) {
+        Employee.findOneAndUpdate({ _id: req.body._id }, req.body, { new: true }, (err, doc) => {
+            if (!err) { res.redirect('viewemployee'); }
+            else {
+                    res.render("employee", {
+                        viewTitle: 'Update Employee',
+                        employee: req.body
+                    });
+            }
+        });
+    }    
 
 app.get('/delete/:id', (req,res) =>{
     Employee.findByIdAndRemove(req.params.id,(err,data)=>{
@@ -58,10 +83,14 @@ app.get('/delete/:id', (req,res) =>{
     });
 });
 
-app.get('/delete/:id', (req,res) =>{
-    Employee.findByIdAndRemove(req.params.id,(err,data)=>{
-        if (err) throw err;
-        res.redirect('/viewemployee');
+app.get('/:id', (req, res) => {
+    Employee.findById(req.params.id, (err, data) => {
+        if (!err) {
+            res.render("employee", {
+                viewTitle: "Update Employee",
+                employee: data
+            });
+        }
     });
 });
 
